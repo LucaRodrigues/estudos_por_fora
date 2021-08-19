67fa2c03ace3d682c28d3141f035d1a99188559c
Luca Rodrigues <74923223+LucaRodrigues@users.noreply.github.com>
2021-08-19 17:34:37 -0300

    Update arrays.php

--- Curso_PHP/Php_Base/arrays.php
+++ Curso_PHP/Php_Base/arrays.php
@@ -9,43 +9,41 @@
 		<h2>ARRAYS</h2>
 		<hr>
 		<small>Curso de Básico de PHP - Aluno: Luca Rodrigues</small>
+		<?php $cursos = array("Excel"=> array("n_alunos" => 400,
+                                       "titulo" => "Do zero ao avançado",
+                                       "url" => "http://udemy.com",
+                                       "aval"=> 200), 
+                                       
+                                       "HTML-CSS"=> array("n_alunos" => 300,
+                                       "titulo" => "Aprenda Desenvolvimento Web",
+                                       "url" => "http://udemy.com",
+                                       "aval"=> 150), 
 
+                                       "Python"=> array("n_alunos" => 280,
+                                       "titulo" => "Programe em Python",
+                                       "url" => "http://udemy.com",
+                                       "aval"=> 90)
+                            );  ?>
 		
-		<h3>Meus Cursos</h3>
-
-			<h4>Conteúdo do Array: </h4>
-			<p><?php  ?></p>
-			<br>
-
 
 		<h3>Informação do Curso</h3>
 
 			<h4>Título: </h4>
-			<p><?php  ?></p>
+			<p><?php echo $cursos['Excel'] ['titulo']?></p>
 			<br>
 
-			<h4>Número de Avaliações: </h4>
-			<p><?php  ?></p>
+			<h4>Número de alunos: </h4>
+			<p><?php echo $cursos['Excel'] ['n_alunos'] ?></p>
+			<br>
+
+			<h4>Número de avaliações: </h4>
+			<p><?php echo $cursos['Excel'] ['aval'] ?></p>
 			<br>
 
 			<h4>URL: </h4>
-			<p><?php  ?></p>
-			<br>
-
-			<h4>URL da foto: </h4>
-			<p><?php  ?></p>
+			<p><?php echo $cursos['Excel'] ['url'] ?></p>
 			<br>
 		
-
-		<h3>Agora é a sua vez</h3>
-
-			<p>Crie um Array e solte as suas informações em sequência. Pesquise também funções que podem ser aplicadas neste tipo de dados.</p>
-			<br>
-
-
-
-
-
 
 		<?php include 'functions/bottom_index.php'; ?>
 
