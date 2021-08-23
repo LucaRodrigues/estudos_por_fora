95bbcc6e860e21e08d5432860c893eefff1b7ab5
Luca Rodrigues <74923223+LucaRodrigues@users.noreply.github.com>
2021-08-23 15:18:50 -0300

    Update loops-for-while.php

--- Curso_PHP/Php_Base/loops-for-while.php
+++ Curso_PHP/Php_Base/loops-for-while.php
@@ -2,8 +2,6 @@
 		include 'header.php';
 		$aula_atual = 'loops-for-while';
 	?>
-
-
 	<body>
 
 		<h2>LOOPS WHILE e FOR</h2>
@@ -12,49 +10,27 @@
 
 		<h3>Selecione o ano de nascimento:</h3>
 
+		<?php $ano_atual = date('Y');
+			  $ano = $ano_atual;
+			  ?>
+
 		<select>
 			<option>Selecione o ano</option>
-			<option>2017</option>
-			<option>2016</option>
-			<option>2015</option>
+			<?php while ($ano > 1980) {?>
+				<option><?php echo $ano; ?></option>
+				<?php $ano = $ano -1; ?>
+			<?php }?>
 		</select>
 
 		<h3>Selecione o ano estimado de aposentadoria:</h3>
 
 		<select>
 			<option>Selecione o ano</option>
-			<option>2018</option>
-			<option>2019</option>
-			<option>2020</option>
-			<option>2021</option>
-			<option>2022</option>
+			<?php for ($aposen = $ano_atual; $aposen < 2050; $aposen = $aposen+1){?>
+				<option><?php echo $aposen;?></option>
+			<?php }?>
 		</select>
 
-			
-
-		<h3>Agora é a sua vez.</h3>
-
-		<p>Use o loop for ou while para apresentar os anos em que vão haver Copa do mundo e Olímpiadas, até 2050.</p>
-
-		<h4>Anos de Copa do Mundo: </h4>
-
-		<p>apresente aqui os anos</p>
-		<br>
-
-		<h4>Anos de Olimpíadas: </h4>
-
-		<p>apresente aqui os anos</p>
-		<br>
-
-		
-
-
-
-
-
 		<?php include 'functions/bottom_index.php'; ?>
-
-
 	</body>
-
 </html>
